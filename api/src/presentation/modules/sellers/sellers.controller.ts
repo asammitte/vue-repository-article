ca0f178df13d91133d4b8e6cdeff01dd3a2426fc
Kostya <pavli4enko30@gmail.com>
2023-08-16 18:10:39 +0200

    remove commented code

--- api/src/presentation/modules/sellers/sellers.controller.ts
+++ api/src/presentation/modules/sellers/sellers.controller.ts
@@ -1,10 +1,7 @@
-import OrderDirectionEnum from '@/domain/enums/order-direction.enum'
-import SellerSortfieldEnum from '@/domain/enums/seller-sortfield.enum'
+import { IGetPaginatedSellers } from '@/presentation/modules/sellers/dto/get-paginated-sellres.dto'
+import { NextFunction, Request, Response } from "express";
 import sellerCreateCommand from '@/application/sellers/commands/create-seller.service'
 import sellerGetAllQuery from '@/application/sellers/queries/getPaginated/get-paginated-sellers.service'
-import GetPaginatedSellersDto, { IGetPaginatedSellers } from '@/presentation/modules/sellers/dto/get-paginated-sellres.dto'
-
-import { NextFunction, Request, Response } from "express";
 
 const getAll = async (
   req: Request,
@@ -30,27 +27,7 @@ async function create(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-// async function update(req, res, next) {
-//   try {
-//     res.json(await programmingLanguages.update(req.params.id, req.body));
-//   } catch (err) {
-//     console.error(`Error while updating programming language`, err.message);
-//     next(err);
-//   }
-// }
-
-// async function remove(req, res, next) {
-//   try {
-//     res.json(await programmingLanguages.remove(req.params.id));
-//   } catch (err) {
-//     console.error(`Error while deleting programming language`, err.message);
-//     next(err);
-//   }
-// }
-
 export default {
   getAll,
   create,
-  // update,
-  // remove
 }
--- api/src/presentation/modules/sellers/sellers.router.ts
+++ api/src/presentation/modules/sellers/sellers.router.ts
@@ -5,16 +5,6 @@ import paginatedSellersDto from './dto/get-paginated-sellres.dto'
 
 const router = express.Router();
 
-/* GET programming languages. */
 router.get('/', validateDto(paginatedSellersDto), sellersController.getAll)
-
-/* POST programming language */
-// router.post('/', sellersController.create)
-
-/* PUT programming language */
-// router.put('/:id', programmingLanguagesController.update)
-
-/* DELETE programming language */
-// router.delete('/:id', programmingLanguagesController.remove)
 
 export default router
