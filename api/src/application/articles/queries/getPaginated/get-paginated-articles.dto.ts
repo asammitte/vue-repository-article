4384c551cef3b5240cd3f6c492c802607ee8b35c
Kostya <pavli4enko30@gmail.com>
2023-08-23 20:33:32 +0200

    add author name to paginatedArticles response

--- api/src/application/articles/queries/getPaginated/get-paginated-articles.dto.ts
+++ api/src/application/articles/queries/getPaginated/get-paginated-articles.dto.ts
@@ -1,6 +1,7 @@
-class GetPaginatedArticlesDto {
+export default class GetPaginatedArticlesDto {
   public id: number
   public title: string
   public content: string
+  public authorName: string
   public totalLikes: number
 }
