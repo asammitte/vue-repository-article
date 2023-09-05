7220af4e9c4bf5320839cf7b24433e72eb777233
Kostya <pavli4enko30@gmail.com>
2023-09-05 17:05:24 +0200

    remove mergeParams from swagger router

--- api/src/presentation/apidoc/swagger.router.ts
+++ api/src/presentation/apidoc/swagger.router.ts
@@ -3,7 +3,7 @@ import { apiDocumentationV2 } from '@/presentation/apidoc/docs/api-v2.doc'
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 
-const router = express.Router({ mergeParams: true })
+const router = express.Router()
 
 router.use('/v1', swaggerUi.serveFiles(apiDocumentationV1), swaggerUi.setup(apiDocumentationV1))
 router.use('/v2', swaggerUi.serveFiles(apiDocumentationV2), swaggerUi.setup(apiDocumentationV2))
