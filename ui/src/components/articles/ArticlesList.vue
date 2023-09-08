c1b76aa9544dd8c530b8b40ea44e96b47d51fd91
Kostya <pavli4enko30@gmail.com>
2023-09-08 21:50:53 +0200

    delete unused imports

--- ui/src/components/articles/ArticlesList.vue
+++ ui/src/components/articles/ArticlesList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 // import axios from 'axios'
 import type { IArticleListItem } from '@/interfaces/articles/IArticleListItem'
 import { useHttp } from '@/plugins/api'
