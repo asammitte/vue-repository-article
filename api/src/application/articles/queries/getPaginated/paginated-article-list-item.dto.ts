b7385a49867d3e025214d5dee592c19940597bab
Kostya <pavli4enko30@gmail.com>
2023-09-08 22:26:57 +0200

    remove dry code

--- api/src/application/articles/queries/getPaginated/paginated-article-list-item.dto.ts
+++ api/src/application/articles/queries/getPaginated/paginated-article-list-item.dto.ts
@@ -1,21 +1,9 @@
 export default class PaginatedArticleListItemDto {
-  public id: number
-  public title: string
-  public content: string
-  public authorName: string
-  public totalLikes: number
-
   constructor(
-    id: number,
-    title: string,
-    content: string,
-    authorName: string,
-    totalLikes: number
-  ) {
-    this.id = id
-    this.title = title
-    this.content = content
-    this.authorName = authorName
-    this.totalLikes = totalLikes
-  }
+    public id: number,
+    public title: string,
+    public content: string,
+    public authorName: string,
+    public totalLikes: number
+  ) {}
 }
