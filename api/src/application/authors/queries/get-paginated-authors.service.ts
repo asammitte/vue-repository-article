214d8af0cc2e05e7e51ab77eb6b383cb98e1c71b
Kostya <pavli4enko30@gmail.com>
2023-09-08 21:49:18 +0200

    introduce paginated authors service

---
+++ api/src/application/authors/queries/get-paginated-authors.service.ts
@@ -0,0 +1,25 @@
+import { IAuthorsRepository } from "@/application/interfaces/IAuthorsRepository"
+import AuthorSortfieldEnum from "@/domain/enums/author-sortfield.enum"
+import OrderDirectionEnum from "@/domain/enums/order-direction.enum"
+import PaginatedAuthorListItemDto from "@/application/authors/queries/paginated-author-list-item.dto"
+
+export const getPaginatedAuthors = (authorsRepository: IAuthorsRepository) => async (
+  pageIndex: number = 1,
+  pageSize: number = 10,
+  sortfield: AuthorSortfieldEnum = AuthorSortfieldEnum.FirstName,
+  orderDirection: OrderDirectionEnum = OrderDirectionEnum.Asc
+): Promise<PaginatedAuthorListItemDto[]> => {
+  const source = await authorsRepository.getAll(pageIndex, pageSize, sortfield, orderDirection)
+
+  const authors: PaginatedAuthorListItemDto[] = []
+  source.forEach(a => {
+    const author = new PaginatedAuthorListItemDto(
+      a.id, a.firstName, a.lastName, a.rating
+    )
+    authors.push(author)
+  })
+
+  return authors
+}
+
+export default getPaginatedAuthors
---
+++ api/src/application/authors/queries/paginated-author-list-item.dto.ts
@@ -0,0 +1,8 @@
+export default class PaginatedAuthorListItemDto {
+  constructor(
+    id: number,
+    firstName: string,
+    lastName: string,
+    rating: number
+  ) {}
+}
