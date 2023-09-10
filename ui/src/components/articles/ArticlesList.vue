e1254b1aecf117391d5ec2973a0a548b33c4c726
Kostya <pavli4enko30@gmail.com>
2023-09-10 21:58:57 +0200

    refactor articles templates

--- ui/src/components/articles/ArticlesList.vue
+++ ui/src/components/articles/ArticlesList.vue
@@ -1,29 +1,97 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
 import type { IArticleListItem } from '@/interfaces/articles/IArticleListItem'
-import { useHttp } from '@/plugins/api'
-import ArticlesListItem from './ArticlesListItem.vue'
 
-const api = useHttp()
-const articles = ref<IArticleListItem[]>([])
+interface IProps{
+  articles: IArticleListItem[]
+}
 
-onMounted((): void => {
-  fetchArticles()
-})
+const props = defineProps<IProps>()
 
-const fetchArticles = async (): Promise<void> => {
-  await api.articles.getAll()
-    .then(data => articles.value = data)
-    .catch(ex => console.log(ex.code))
+const getDescription = (source: string, size: number = 80): string => {
+  return source.length > size ? source.slice(0, size - 1) + "…" : source
 }
 </script>
 
 <template>
   <div class="c-articles-list">
-    <ArticlesListItem
-      v-for="article in articles"
+    <div
+      v-for="article in props.articles"
       :key="article.id"
-      :article="article"
-    />
+      class="item"
+    >
+      <div class="article-details">
+        <h3>
+          {{ article.title }}
+        </h3>
+        <div>
+          <div>
+            {{ getDescription(article.content) }}
+          </div>
+          <div class="article-props">
+            <div class="article-prop">Author: <b>{{ article.authorName }}</b></div>
+            <div class="article-prop">Likes: <b>{{ article.totalLikes }}</b></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.item {
+  display: flex;
+  align-items: center;
+  position: relative;
+  border-bottom: 1px solid var(--color-border);
+  cursor: default;
+  padding: 0.4rem 0 1rem 0;
+}
+
+.article-details {
+  flex: 1;
+}
+
+.article-avatar-wrapper {
+  flex: 0;
+}
+
+.article-avatar {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 8px;
+  text-transform: uppercase;
+
+  color: black;
+  background-color: var(--color-border-hover);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+.article-prop {
+  display: inline-block;
+  padding: 0 1rem;
+  font-size: 0.8rem;
+}
+
+.article-prop:first-child {
+  padding-left: 0;
+}
+
+.article-prop:not(:first-child) {
+  border-left: 1px solid var(--color-border);
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+  }
+}
+</style>
--- ui/src/views/ArticlesView.vue
+++ ui/src/views/ArticlesView.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
+import type { IArticleListItem } from '@/interfaces/articles/IArticleListItem';
+import { onMounted, ref } from 'vue'
+import { useHttp } from '@/plugins/api'
 import ArticlesList from '@/components/articles/ArticlesList.vue'
 import PopularAuthors from '@/components/authors/PopularAuthors.vue'
+
+const api = useHttp()
+const articles = ref<IArticleListItem[]>([])
+
+onMounted((): void => {
+  fetchArticles()
+})
+
+const fetchArticles = async (): Promise<void> => {
+  await api.articles.getAll()
+    .then(data => articles.value = data)
+    .catch(ex => console.log(ex.code))
+}
 </script>
 
 <template>
   <div class="c-page c-articles-page">
     <div class="page-content-main">
-      <ArticlesList />
+      <ArticlesList :articles="articles" />
     </div>
     <div class="page-content-right">
       <PopularAuthors />
